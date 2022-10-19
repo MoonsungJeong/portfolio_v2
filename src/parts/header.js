b8b82a3ada2f43411256e142fa5ee33e4b2f6bfa
Moonsungjeong <gmsj159@gmail.com>
2022-10-19 20:20:07 +1000

    header nav interaction update

--- src/parts/header.js
+++ src/parts/header.js
@@ -7,6 +7,7 @@ export default function Header(props) {
     window.addEventListener("popstate", function () {
       initNavHeader();
     });
+    initNavHeader();
   });
   // header nav initialize
   function initNavHeader() {
@@ -23,6 +24,7 @@ export default function Header(props) {
           element.classList.remove("active");
         });
         item.classList.add("active");
+        props.onChangeMode(pathPrevious);
       }
     });
   }
@@ -33,14 +35,57 @@ export default function Header(props) {
     list.forEach((item) => item.classList.remove("active"));
     target.classList.add("active");
   }
+
+  let list = [];
+  const menu = ["Home", "Portfolio", "Contact", "CV"];
+  for (let i = 0; i < 4; i++) {
+    if (menu[i] === props.path) {
+      list.push(
+        <li key={i}>
+          <Link
+            to={menu[i]}
+            className="header-list active"
+            onClick={(event) => {
+              activeNavHeader(event);
+              props.onChangeMode(menu[i]);
+            }}
+          >
+            {menu[i]}
+          </Link>
+        </li>
+      );
+    } else {
+      list.push(
+        <li key={i}>
+          <Link
+            to={menu[i]}
+            className="header-list"
+            onClick={(event) => {
+              activeNavHeader(event);
+              props.onChangeMode(menu[i]);
+            }}
+          >
+            {menu[i]}
+          </Link>
+        </li>
+      );
+    }
+  }
   return (
     <header>
-      <Link to="/" className="logo">
+      <Link
+        to="/"
+        className="logo"
+        onClick={(event) => {
+          props.onChangeMode("Home");
+        }}
+      >
         Moonsung
       </Link>
       <nav>
         <ul>
-          <li>
+          {list}
+          {/* <li>
             <Link
               to="/"
               className="header-list active"
@@ -82,12 +127,12 @@ export default function Header(props) {
               className="header-list"
               onClick={(event) => {
                 activeNavHeader(event);
-                props.onChangeMode("Cv");
+                props.onChangeMode("CV");
               }}
             >
               CV
             </Link>
-          </li>
+          </li> */}
         </ul>
       </nav>
     </header>
